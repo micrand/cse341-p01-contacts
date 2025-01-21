3fadda09fa82a74c78e4483346f79149973bcf59
mickaelrmt <mickael@rmt-itsolutions.com>
2025-01-21 22:28:52 +0300

    feat: get and get all endpoints1

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const path = require('path');
 
+const mongodb = require('./data/database');
+
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+
+
 app
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .use((req, res, next) => {
-        // handle 404 page
-        res.render('pages/404', { title: '404 - Page Not Found', path: req.url });
-    })
-    .listen(PORT, () => {
-        console.log(`Server running on ${PORT}`)
-    })
+    .set('view engine', 'ejs')    
+    // .use((req, res, next) => {
+    //     // handle 404 page
+    //     res.render('pages/404', { title: '404 - Page Not Found', path: req.url });
+    // })
+
+app
+    .use('/', require('./routes'))
+    .use('/users', require('./routes/users'))
+
+mongodb.initDb( (err)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        app.listen(PORT, () => {
+            console.log(`Database is listening and node running on ${PORT}`)
+        })
+    }
+})
+    
