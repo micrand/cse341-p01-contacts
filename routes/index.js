481a01a57b7be0ad08bc055a7f329e85c01a46ed
mickaelrmt <mickael@rmt-itsolutions.com>
2025-01-21 22:50:37 +0300

    change: default text

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@
 const express = require('express');
 const router = express.Router()
 
-router.get('/', (req, res) => { res.send('Hello world')} )
+router.get('/', (req, res) => { res.send('CSE W01 Project')} )
 
 module.exports = router
